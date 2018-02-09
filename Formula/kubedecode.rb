e7cac02e9bef30c79c064dfec4b890b900577ba3
Melissa Marshall <mveritym@gmail.com>
2018-02-09 13:57:31 +0000

    Adding kubedecode formula

---
+++ Formula/kubedecode.rb
@@ -0,0 +1,12 @@
+class Kubedecode < Formula
+  desc "Decode all parts of a kubernetes secret, no more copypasting!"
+  homepage "https://github.com/mveritym/kubedecode"
+  url "https://github.com/mveritym/kubedecode/archive/v1.3.tar.gz"
+  sha256 "bf6c18124c6e018d291bb8f999c39ec7655904c9909841f2eeffcc2b61d2ddaf"
+
+  depends_on "jq" => :run
+
+  def install
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+end
